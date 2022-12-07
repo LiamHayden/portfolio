5f24b387cef2bcfe1b1144a74cfd827c963b4b58
Liam Hayden <l.hayden67@outlook.com>
2022-12-07 17:44:45 +0000

    Add navbar and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'portfolios/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'portfolios#index'
+  
+  resources 'portfolios'
 end
