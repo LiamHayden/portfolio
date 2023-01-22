b2a8463385a7d38a02f09eaac235dfc2140915fc
Liam Hayden <l.hayden67@outlook.com>
2023-01-22 12:17:12 +0000

    Add turbo frame, create view and edit form tag

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,7 +15,6 @@ class MessagesController < ApplicationController
     if @message.save
       flash[:success] = "Your message has successfully been sent! 
       You should receive an email confirmation soon."
-      redirect_to root_path
     else
       flash.now[:error] = "Please fix the errors."
       render :new, status: :unprocessable_entity
