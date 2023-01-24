befa1983cfcf41c16702a4147deeff6a1640678e
Liam Hayden <l.hayden67@outlook.com>
2023-01-24 10:58:00 +0000

    Add mailer to messages#create, add bcc, and update email

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,9 +1,9 @@
 class MessageMailer < ApplicationMailer
   default to: -> { Admin.pluck(:email) },
-          from: 'notification@webdesigns.com'
+          from: 'notifications@webdesigns.com'
 
   def confirmation_email
     @message = params[:message]
-    mail(to: @message.email, subject: 'Confirmation Email')
+    mail(to: @message.email, bcc:Admin.pluck(:email), subject: 'Confirmation Email')
   end
 end
